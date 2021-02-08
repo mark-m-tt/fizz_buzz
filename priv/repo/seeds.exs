d4791db4ad108913af9ca047c40adc51caf95c42
Mark M <markmekhaiel@gmail.com>
2021-02-08 19:31:12 +0000

    Remove seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,13 +1,13 @@
 # Create a default user with some favourites
 
-alias FizzBuzz.Repo
-alias FizzBuzz.Accounts
+# alias FizzBuzz.Repo
+# alias FizzBuzz.Accounts
 
-attrs = %{username: "username", password: "password", password_confirmation: "password"}
-user = %Accounts.User{} |> Accounts.User.changeset(attrs) |> Repo.insert!()
+# attrs = %{username: "username", password: "password", password_confirmation: "password"}
+# user = %Accounts.User{} |> Accounts.User.changeset(attrs) |> Repo.insert!()
 
-Enum.each([3, 5, 10], fn number ->
-  %Accounts.Favourite{}
-  |> Accounts.Favourite.changeset(%{number: number, user_id: user.id})
-  |> Repo.insert!()
-end)
+# Enum.each([3, 5, 10], fn number ->
+#   %Accounts.Favourite{}
+#   |> Accounts.Favourite.changeset(%{number: number, user_id: user.id})
+#   |> Repo.insert!()
+# end)
