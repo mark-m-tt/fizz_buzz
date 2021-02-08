5da31d044ba903eae3a8463ab3e6602f83ad58a3
Mark M <markmekhaiel@gmail.com>
2021-02-08 12:59:52 +0000

    Setup all basic API client actions

--- client/client.rb
+++ client/client.rb
@@ -30,12 +30,30 @@ class Client
     self.class.post(url, body: body)
   end
 
+  def play(starting_number: 1, count: 15)
+    url = "#{API_URL}/play?starting_number=#{starting_number}&count=#{count}"
+    self.class.get(url).parsed_response
+  end
+
+  def list_favourites
+    self.class.get(FAVOURITES_URL, headers: auth_header).parsed_response
+  end
+
+  def make_favourite(number:)
+    self.class.post(FAVOURITES_URL, body: { number: number}, headers: auth_header).parsed_response
+  end
+
+  def remove_favourite(number:)
+    id = get_favourite_id_from_number(number: number)
+    self.class.delete("#{FAVOURITES_URL}/#{id}", body: { number: number}, headers: auth_header).parsed_response
+  end
+
   # private
 
   attr_reader :username, :password
 
   def auth_header
-    "Bearer #{jwt}"
+    { "Authorization": "Bearer #{jwt}" }
   end
 
   def jwt
@@ -47,5 +65,10 @@ class Client
       }
     }
     self.class.post(url, body: body).parsed_response["jwt"]
+  end
+
+  def get_favourite_id_from_number(number:)
+    url = "#{FAVOURITES_URL}/find_by_number/#{number}"
+    self.class.get(url, headers: auth_header).parsed_response.dig("favourite", "id")
   end
 end
